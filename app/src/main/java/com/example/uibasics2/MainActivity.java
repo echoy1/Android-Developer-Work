3943869715d81312dcd5920390a5aaf7a3aef7e8
Eric <echoy1@terpmail.umd.edu>
2021-05-20 16:28:19 -0400

    add app

---
+++ app/src/main/java/com/example/uibasics2/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.uibasics2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnHello:
+                Toast.makeText(this, "Hello Button Clicked", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnHello = findViewById(R.id.btnHello);
+        btnHello.setOnClickListener(this);
+
+        /*btnHello.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                Toast.makeText(MainActivity.this, "Long Press", Toast.LENGTH_LONG).show();
+                return true;
+            }
+        });*/
+    }
+}
