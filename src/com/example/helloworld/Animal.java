0a85a855dcf8fff6d3c93e3fef29cce28592f88b
Eric <echoy1@terpmail.umd.edu>
2021-05-20 01:23:47 -0400

    Removed extra if

---
+++ src/com/example/helloworld/Animal.java
@@ -0,0 +1,10 @@
+package com.example.helloworld;
+
+public class Animal {
+    private String name;
+    private String color;
+    private int legs;
+    private boolean hasTail;
+}
+
+
---
+++ src/com/example/helloworld/Hello.java
@@ -0,0 +1,45 @@
+package com.example.helloworld;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Hello {
+    public static void main(String[] args) {
+        String answer;
+        System.out.println("Welcome!");
+        System.out.println("What is your name?");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.next();
+        System.out.println("Hello " + name);
+
+        do {
+            System.out.println("If you would like to start, say yes.");
+            answer = scanner.next();
+        } while (!answer.equals("yes"));
+
+        Random random = new Random();
+        int number = random.nextInt(100 + 1);
+
+        System.out.println("Please guess the number I'm thinking of between 1 and 100");
+        int guesses = 0;
+        while (guesses < 5) {
+            int guess = scanner.nextInt();
+            if (guess == number) {
+                System.out.println("Congratulations!!");
+                break;
+            } else {
+                if (guess > number) {
+                    System.out.println("Guess lower!");
+                } else {
+                    System.out.println("Guess higher!");
+                }
+                guesses++;
+            }
+            if (guesses == 5) {
+                System.out.println("Game Over");
+                System.out.println("The number was " + number);
+                break;
+            }
+        }
+    }
+}
